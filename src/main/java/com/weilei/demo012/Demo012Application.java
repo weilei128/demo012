97a4f46cdf2295c3cec761e78faf65bce0c7b23e
卫磊 <17338115070@163.com>
2018-06-22 11:05:21 +0800

    第二次提交

---
+++ src/main/java/com/weilei/demo012/Demo012Application.java
@@ -0,0 +1,12 @@
+package com.weilei.demo012;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo012Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo012Application.class, args);
+	}
+}
